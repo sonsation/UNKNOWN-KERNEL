804c28ce7b92294874b60a4f02196b789ae47df9
sonsation <jhson9304@gmail.com>
2017-11-29 00:13:06 +0900

    sched/clock:sched_clock_cpu , need to local IRQs disabled
    Signed-off-by: sonsation <jhson9304@gmail.com>

--- kernel/sched/clock.c
+++ kernel/sched/clock.c
@@ -242,20 +242,20 @@ u64 sched_clock_cpu(int cpu)
 	struct sched_clock_data *scd;
 	u64 clock;
 
-	WARN_ON_ONCE(!irqs_disabled());
-
 	if (sched_clock_stable)
 		return sched_clock();
 
 	if (unlikely(!sched_clock_running))
 		return 0ull;
 
+	preempt_disable_notrace();
 	scd = cpu_sdc(cpu);
 
 	if (cpu != smp_processor_id())
 		clock = sched_clock_remote(scd);
 	else
 		clock = sched_clock_local(scd);
+	preempt_enable_notrace();
 
 	return clock;
 }
